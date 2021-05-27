aca251516623a83e57524f1318efdbe175312519
Logan Thomas <logannormanthomas@protonmail.com>
2021-05-27 03:19:30 +0000

    added a property add feature with a sql query through node

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -174,14 +174,23 @@ exports.getAllProperties = getAllProperties;
 
 
 /**
- * ADD A PROPERTY TO THE DATABASE
- * @PARAM {{}} PROPERTY AN OBJECT CONTAINING ALL OF THE PROPERTY DETAILS.
- * @RETURN {PROMISE<{}>} A PROMISE TO THE PROPERTY.
+ * Add a property to the database
+ * @param {{}} property An object containing all of the property details.
+ * @return {Promise<{}>} A promise to the property.
  */
-// const addProperty = function(property) {
-//   const propertyId = OBJECT.KEYS(PROPERTIES).LENGTH + 1;
-//   PROPERTY.ID = PROPERTYID;
-//   PROPERTIES[PROPERTYID] = PROPERTY;
-//   return promise.resolve(property);
-// }
-// exports.addProperty = addProperty;
+ const addProperty = function(property) {
+  
+let queryString = `
+  INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+`
+let queryParams = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms];
+
+console.log(queryString, queryParams);
+return pool.query(queryString, queryParams).then((res) => {
+  return res.rows;
+})
+
+};
+exports.addProperty = addProperty;
